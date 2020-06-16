d4dc4bdfd32eadace8cd1389fbd09a69a83f37b3
Victoria  Topham <topham.victoria@gmail.com>
2020-06-16 09:29:43 -0700

    test for agnostic binary search is passing

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -10,7 +10,7 @@ def binary_search(arr, target, floor, ceiling):
     elif target < arr[midpoint]:
         return binary_search(arr, target, floor, midpoint + 1)
     elif target > arr[midpoint]:
-        return binary_search(arr, target, midpoint, ceiling - 1)
+        return binary_search(arr, target, midpoint - 1, ceiling)
     
 
 
@@ -22,5 +22,35 @@ def binary_search(arr, target, floor, ceiling):
 # You can implement this function either recursively 
 # or iteratively
 def agnostic_binary_search(arr, target):
-    # Your code here
-    pass
+    
+    #Get our start and end in place. Because this isn't supplied in the arguments, i don't think we can go recursive.
+    start = 0
+    end = len(arr) - 1
+    
+    #find out of the array is ascending or descending
+    if arr[start] < arr[end]:
+        ascending = True
+        print("array is ascending")
+    else:
+        ascending = False
+
+    #Loop until we find the target
+    while start <= end:
+        #Set the midpoint
+        midpoint = (end - start) // 2 + start
+        #If we found the target, return
+        if target == arr[midpoint]:
+            return midpoint
+        #else let's move the midpoint
+        elif target < arr[midpoint]:
+            #check if it's ascending or descending, because that will change which variable we move
+            if ascending:
+                end = midpoint -1
+            else:
+                start = midpoint + 1
+        elif target > arr[midpoint]:
+            if ascending:
+                start = midpoint + 1
+            else:
+                end = midpoint - 1
+    return -1
